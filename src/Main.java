a71531cdf102b23a42cc0f798de4645efa3d1ed2
Victor Bäckström <victor.backstrom@outlook.com>
2018-10-24 13:59:03 +0200

    HEEEEEJETESST DAVID

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hejhej");
+        
+        
+        
+    }
+    
+    
+}
+
+
